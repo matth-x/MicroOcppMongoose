c49e65b94aff3122b952582381ff82ea55d27a29
matth-x <63792403+matth-x@users.noreply.github.com>
2022-11-26 20:48:27 +0100

    ESP-IDF integration fixes

--- src/AOcppMongooseClient_c.h
+++ src/AOcppMongooseClient_c.h
@@ -17,27 +17,27 @@
 extern "C" {
 #endif
 
-struct AOcppSocketHandle;
-typedef struct AOcppSocketHandle AOcppSocketHandle;
+struct AOcppSocket;
+typedef struct AOcppSocket AOcppSocket;
 
-AOcppSocketHandle *ao_makeOcppSocket(struct mg_mgr *mgr,
+AOcppSocket *ao_makeOcppSocket(struct mg_mgr *mgr,
         const char *backend_url_default,   //all cstrings can be NULL
         const char *charge_box_id_default,
         const char *auth_key_default,
         const char *CA_cert_default, //if AO_CA_CERT_USE_FILE, then pass the filename, otherwise the plain-text CA_cert
-        AO_FilesystemOpt fsopt);
+        struct AO_FilesystemOpt fsopt);
 
-void ao_setBackendUrl(AOcppSocketHandle *sock, const char *backend_url);
-void ao_setChargeBoxId(AOcppSocketHandle *sock, const char *cb_id);
-void ao_setAuthKey(AOcppSocketHandle *sock, const char *auth_key);
-void ao_setCaCert(AOcppSocketHandle *sock, const char *ca_cert); //if AO_CA_CERT_USE_FILE, then pass the filename, otherwise the plain-text CA_cert
+void ao_setBackendUrl(AOcppSocket *sock, const char *backend_url);
+void ao_setChargeBoxId(AOcppSocket *sock, const char *cb_id);
+void ao_setAuthKey(AOcppSocket *sock, const char *auth_key);
+void ao_setCaCert(AOcppSocket *sock, const char *ca_cert); //if AO_CA_CERT_USE_FILE, then pass the filename, otherwise the plain-text CA_cert
 
-void ao_reconnect(AOcppSocketHandle *sock); //after updating all credentials, reconnect to apply them
+void ao_reconnect(AOcppSocket *sock); //after updating all credentials, reconnect to apply them
 
-const char *ao_getBackendUrl(AOcppSocketHandle *sock);
-const char *ao_getChargeBoxId(AOcppSocketHandle *sock);
-const char *ao_getAuthKey(AOcppSocketHandle *sock);
-const char *ao_getCaCert(AOcppSocketHandle *sock);
+const char *ao_getBackendUrl(AOcppSocket *sock);
+const char *ao_getChargeBoxId(AOcppSocket *sock);
+const char *ao_getAuthKey(AOcppSocket *sock);
+const char *ao_getCaCert(AOcppSocket *sock);
 
 #ifdef __cplusplus
 }
